cf3dfbadd8a499841d938dde601c63b82286583f
abhinav-TB <abhinavtb@gmail.com>
2020-03-04 19:45:29 +0530

    save

--- services/curation.js
+++ services/curation.js
@@ -218,7 +218,7 @@ module.exports = class Curation {
     }
 
     let response = Response.genGenericTemplate(
-      `${config.appUrl}/styles/${outfit}.jpg`,
+      `${config.appUrl}/styles/item-1.jpg`,
       i18n.__("curation.title"),
       i18n.__("curation.subtitle"),
       buttons
